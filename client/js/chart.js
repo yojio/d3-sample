ce5b47e610ec486b38725fc689a0a53fa20ff08c
yojio <yojio.jp@gmail.com>
2015-03-17 20:26:37 +0900

    update

--- client/js/chart.js
+++ client/js/chart.js
@@ -2,13 +2,16 @@
  * Created by yoji on 15/03/09.
  */
 var DEF_OPT = {
-  dom: ""           // 表示用DIV
-  , width: 100      // サイズ横
-  , height: 100     // サイズ縦
-  , title: {       // タイトル情報
-    caption: ""
-    , height: 20
-    , fontSize: 16
+  dom : "" // 表示用DIV
+  ,
+  width : 100 // サイズ横
+  ,
+  height : 100 // サイズ縦
+  ,
+  title : { // タイトル情報
+    caption : "",
+    height : 20,
+    fontSize : 16
   }
 };
 
@@ -19,159 +22,149 @@ function Chart(option) {
 };
 
 // 描画
-Chart.prototype.draw = function (data) {
+Chart.prototype.draw = function(data) {
   if (!this.created) {
-    this.create(data);
+    this._create(data);
     this.created = true;
   } else {
-    this.redraw(data);
+    this._redraw(data);
   }
 };
 
 // 作成
-Chart.prototype.create = function (data) {
+Chart.prototype._create = function(data) {
   throw new Error("Unimplemented");
 };
 
 // 再描画
-Chart.prototype.redraw = function (data) {
-  alert("bbbb");
+Chart.prototype._redraw = function(data) {
   throw new Error("Unimplemented");
 };
 
 // 円グラフ
 function PieChart(option) {
 
-  var me = this;
-
   Chart.call(this, option);
 
-  // グラフ作成
-  this.create = function (data) {
-
-     me.current = new Array(data.length);;
-
-    // 半径算出
-    var radius = Math.min(me.opt.width, me.opt.height) / 2;
-
-    // 円グラフのサイズを指定
-    var arc = d3.svg.arc().innerRadius(radius / 4).outerRadius(radius - 10);
-
-    // 円グラフを生成
-    var pie = d3.layout.pie().sort(null).value(function (d) {
-      return d[1];
-    });
-
-    // 領域のリセット
-    $(me.dom).empty();
-
-    // 色の用意
-    // var color = d3.scale.category20();
-    // SVGの表示領域を生成
-    var height = (me.opt.title.caption) ? height = me.opt.height + me.opt.title.height : me.opt.height;
-
-    // 規定オブジェクト生成
-    var svg = d3
-        .select(me.opt.dom)
-        .append("svg")
-        .attr("width", me.opt.width)
-        .attr("height", height);
-
-    // タイトル描画
-    if (me.opt.title.caption) {
-      svg.append("text")
-          .attr("class","title")
-          .attr("type","caption")
-          .attr("x", (me.opt.width / 2))
-          .attr("y", me.opt.title.height)
-          .attr("text-anchor", "middle")
-          .style("font-size", me.opt.title.fontSize + "px")
-          .style("text-decoration", "underline")
-          .text(me.opt.title.caption);
-    }
-
-    // 円グラフを描画
-    var g = svg.selectAll("path").data(pie(data)).enter().append("g");
-
-    g.append("path") // 円弧はパスで指定する
-        .attr("d", arc) // 円弧を設定
-        .attr("stroke", "white") // 円グラフの区切り線を白色にする
-        .attr(
-        "transform",
-        "translate(" + me.opt.width / 2 + ", " + ((height / 2) + 20) + ")") // 円グラフをSVG領域の中心にする
-        .style("fill", function (d, i) {
-          return d.data[2];
-          // return color(i);
-        })
-      // 今の数値を保存します。
-        .each(function (d, i) {
-          me.current[i] = d;
-        });
-
-    // データキャプション表示
-    g.append("text")
-        .attr("class","dataCaption")
-        .attr("transform", function (d) {
-          return "translate(" + arc.centroid(d) + ")";
-        })
-        .attr("dx", me.opt.width / 2)
-        .attr("dy", (height / 2) + 20)
-        .style("text-anchor", "middle").text(function (d) {
-          return d.data[0];
-        });
-
-    me.svg = svg;
-    me.arc = arc;
-    me.pie = pie;
-
-  }
-
-  this.redraw = function (data) {
-
-    var svg = me.svg;
-    var pie = me.pie;
-    var arc = me.arc;
-
-    // path
-    svg.selectAll("path")
-    .data(pie(data))
-    .transition() // トランジションを設定するとアニメーションさせることができます。
-    .duration(800) // アニメーションの秒数を設定します。
-    .attrTween("d", function(d,i) { // アニメーションの間の数値を補完します。
-      var interpolate = d3.interpolate(me.current[i], d);
-      me.current[i] = interpolate(0);
-      return function(t) {
-        return arc(interpolate(t));
-      };
-    });
-
-    // キャプション
-    svg.selectAll(".dataCaption")
-    .data(pie(data)) // 新しいデータを設定します。
-    .text(function(d, i) { // 文字を更新します。
-      return d.data[0];
-    })
-    .transition() // トランジションを設定。
-    .duration(800) // アニメーションの秒数を設定。
-    .attrTween( // アニメーションの間の数値を補完。
-        "transform",
-        function(d,i) {
-          var interpolate = d3.interpolate(arc
-              .centroid(me.current[i]), arc
-              .centroid(d));
-          me.current[i] = d;
-          return function(t) {
-            return "translate(" + interpolate(t) + ")";
-          };
-        });
-  };
 };
 
 PieChart.prototype = Object.create(Chart.prototype, {
-  constructor: {
-    value: PieChart,
-    enumerable: false,
-    writable: true,
-    configurable: true
+  constructor : {
+    value : PieChart,
+    enumerable : false,
+    writable : true,
+    configurable : true
+  },
+  // グラフ作成
+  _create : {
+    value : function(data) {
+
+      var me = this;
+
+      me.current = new Array(data.length);
+
+      // 半径算出
+      var radius = Math.min(me.opt.width, me.opt.height) / 2;
+
+      // 円グラフのサイズを指定
+      var arc = d3.svg.arc().innerRadius(radius / 4).outerRadius(radius - 10);
+
+      // 円グラフを生成
+      var pie = d3.layout.pie().sort(null).value(function(d) {
+        return d[1];
+      });
+
+      // 領域のリセット
+      $(me.dom).empty();
+
+      // 色の用意
+      // var color = d3.scale.category20();
+      // SVGの表示領域を生成
+      var height = (me.opt.title.caption) ? height = me.opt.height
+          + me.opt.title.height : me.opt.height;
+
+      // 規定オブジェクト生成
+      var svg = d3.select(me.opt.dom).append("svg").attr("width", me.opt.width)
+          .attr("height", height);
+
+      // タイトル描画
+      if (me.opt.title.caption) {
+        svg.append("text").attr("class", "title").attr("type", "caption").attr(
+            "x", (me.opt.width / 2)).attr("y", me.opt.title.height).attr(
+            "text-anchor", "middle").style("font-size",
+            me.opt.title.fontSize + "px").style("text-decoration", "underline")
+            .text(me.opt.title.caption);
+      }
+
+      // 円グラフを描画
+      var g = svg.selectAll("path").data(pie(data)).enter().append("g");
+
+      g.append("path") // 円弧はパスで指定する
+      .attr("d", arc) // 円弧を設定
+      .attr("stroke", "white") // 円グラフの区切り線を白色にする
+      .attr("transform",
+          "translate(" + me.opt.width / 2 + ", " + ((height / 2) + 20) + ")") // 円グラフをSVG領域の中心にする
+      .style("fill", function(d, i) {
+        return d.data[2];
+        // return color(i);
+      })
+      // 今の数値を保存します。
+      .each(function(d, i) {
+        me.current[i] = d;
+      });
+
+      // データキャプション表示
+      g.append("text").attr("class", "dataCaption").attr("transform",
+          function(d) {
+            return "translate(" + arc.centroid(d) + ")";
+          }).attr("dx", me.opt.width / 2).attr("dy", (height / 2) + 20).style(
+          "text-anchor", "middle").text(function(d) {
+        return d.data[0];
+      });
+
+      me.svg = svg;
+      me.arc = arc;
+      me.pie = pie;
+
+    }
+  },
+  _redraw : {
+    value : function(data) {
+
+      var me = this;
+
+      var svg = me.svg;
+      var pie = me.pie;
+      var arc = me.arc;
+
+      // path
+      svg.selectAll("path").data(pie(data)).transition() // トランジションを設定するとアニメーションさせることができます。
+      .duration(800) // アニメーションの秒数を設定します。
+      .attrTween("d", function(d, i) { // アニメーションの間の数値を補完します。
+        var interpolate = d3.interpolate(me.current[i], d);
+        me.current[i] = interpolate(0);
+        return function(t) {
+          return arc(interpolate(t));
+        };
+      });
+
+      // キャプション
+      svg.selectAll(".dataCaption").data(pie(data)) // 新しいデータを設定します。
+      .text(function(d, i) { // 文字を更新します。
+        return d.data[0];
+      }).transition() // トランジションを設定。
+      .duration(800) // アニメーションの秒数を設定。
+      .attrTween(
+          // アニメーションの間の数値を補完。
+          "transform",
+          function(d, i) {
+            var interpolate = d3.interpolate(arc.centroid(me.current[i]), arc
+                .centroid(d));
+            me.current[i] = d;
+            return function(t) {
+              return "translate(" + interpolate(t) + ")";
+            };
+          });
+    }
   }
 });
--- client/js/main.js
+++ client/js/main.js
@@ -4,21 +4,17 @@
 define([ 'bootstrap', 'underscore', 'backbone', 'd3', 'chart' ],
     function(bootstrap, _, backbone, d3) {
 
-      var list1 = [ [ "項目1", "10", "#98abc5" ], [ "項目2", "30", "#8a89a6" ],
+      var list = [ [ "項目1", "10", "#98abc5" ], [ "項目2", "30", "#8a89a6" ],
           [ "項目3", "20", "#7b6888" ], [ "項目4", "60", "#6b486b" ],
           [ "項目5", "15", "#a05d56" ] ];
-
-      var list2 = [ [ "項目1", "30", "#98abc5" ], [ "項目2", "2", "#8a89a6" ],
-          [ "項目3", "30", "#7b6888" ], [ "項目4", "20", "#6b486b" ],
-          [ "項目5", "20", "#a05d56" ] ];
 
       var me = this;
       me.chartArray = new Array(10);
       for (var i = 0; i < me.chartArray.length; i++) {
         index = i + 1;
         item = "chart" + i;
-
-        $("#result").append('<div id=\"' + item + '\" style=\"float:left\"></div>');
+        $("#result").append(
+            '<div id=\"' + item + '\" style=\"float:left\"></div>');
 
         me.chartArray[i] = createChartObject("#" + item, "サンプルチャート" + index);
       }
@@ -28,16 +24,12 @@ define([ 'bootstrap', 'underscore', 'backbone', 'd3', 'chart' ],
         var activated_tab = e.target; // activated tab
         var previous_tab = e.relatedTarget; // previous tab
         if (activated_tab.hash == "#menu2") {
-          drawChart(list1);
+          drawChart(list);
         }
       });
 
-      $("#data1").on("click", function() {
-        drawChart(list1);
-      });
-
-      $("#data2").on("click", function() {
-        drawChart(list2);
+      $("#dataChange").on("click", function() {
+        drawChart(list);
       });
 
       function createChartObject(dom, caption) {
@@ -56,8 +48,20 @@ define([ 'bootstrap', 'underscore', 'backbone', 'd3', 'chart' ],
       function drawChart(data) {
         // 処理,,,,,
         for (var i = 0; i < me.chartArray.length; i++) {
-          me.chartArray[i].draw(data);
+          me.chartArray[i].draw(changeData(data));
         }
+      }
+
+      function changeData(list) {
+        for (var i = 0; i < list.length; i++) {
+          list[i][1] = getRandomInt(10, 1000);
+        }
+        ;
+        return list;
+      }
+
+      function getRandomInt(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
       }
 
     });
