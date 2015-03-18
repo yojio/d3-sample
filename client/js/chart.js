de7aec1fd8b9ec6b386166fc171ae884235ab250
yojio <yojio.jp@gmail.com>
2015-03-18 17:42:38 +0900

    update

--- client/js/chart.js
+++ client/js/chart.js
@@ -11,6 +11,12 @@ var DEF_OPT = {
     ,fontSize : 16
     ,sorted : false
   }
+  ,axisStyle:{
+    width : 14
+    ,maxValue : 1000
+    ,valueTitle : '値'
+    ,kindTitle : '種類'
+  }
 };
 
 // Chartクラス定義
@@ -80,7 +86,7 @@ PieChart.prototype = Object.create(Chart.prototype, {
       });
 
       // 領域のリセット
-      $(me.dom).empty();
+      $(me.opt.dom).empty();
 
 
       // 色の用意
@@ -205,3 +211,365 @@ PieChart.prototype = Object.create(Chart.prototype, {
     }
   }
 });
+
+// horizontal bar chart
+function HorizontalBarChart(option) {
+
+  Chart.call(this, option);
+
+};
+
+HorizontalBarChart.prototype = Object.create(Chart.prototype, {
+  constructor : {
+    value : PieChart,
+    enumerable : false,
+    writable : true,
+    configurable : true
+  },
+  // グラフ作成
+  _create : {
+    value : function(data) {
+
+      var me = this;
+
+      var titleWidth = 50;
+      var width = me.opt.width - 20 - titleWidth; // 実際の出力サイズ(最後のメモリ分だけバッファを作成）
+      me.barScale = width / me.opt.axisStyle.maxValue;   // 比率
+      var yBarSize = me.opt.axisStyle.width;  // 棒グラフの縦幅
+
+      // 領域のリセット
+      $(me.opt.dom).empty();
+
+      // 規定オブジェクト生成
+      var svg = d3.select(me.opt.dom).append("svg").attr("width", me.opt.width).attr("height", me.opt.height);
+
+      // タイトル描画
+      var top = 0;
+      if (me.opt.title.caption) {
+        svg.append("text").attr("class", "title").attr("type", "caption").attr(
+            "x", (me.opt.width / 2)).attr("y", me.opt.title.height).attr(
+            "text-anchor", "middle").style("font-size",
+            me.opt.title.fontSize + "px").style("text-decoration", "underline")
+            .text(me.opt.title.caption);
+        top = me.opt.title.height +4;
+      }
+      var scaleHeight = data.length*yBarSize +2 + top;
+
+      // 目盛りを表示するためにスケールを設定
+      var xScale = d3.scale.linear()  // スケールを設定
+          .domain([0, me.opt.axisStyle.maxValue])   // 元のサイズ
+          .range([0, width]);
+
+      var yScale = d3.scale.linear()  // スケールを設定
+      .domain([0, data.length * 2])   // 元のサイズ
+      .range([scaleHeight,top]);
+
+      // 目盛りを設定し表示する
+      svg.append("g")
+          .attr("class", "axis")
+          .attr("transform", "translate(" + (8 + titleWidth) + ", "+scaleHeight+")")
+          .call(d3.svg.axis()
+              .scale(xScale)  // スケールを適用する
+              .orient("bottom")   // 目盛りの表示位置を下側に指定
+              .tickPadding(5)
+              .innerTickSize(0)  // 目盛線の長さ（内側）
+              .outerTickSize(0) // 目盛線の長さ（外側）
+          );
+
+      var cnt = 0;
+      svg.append("g")
+      .attr("class", "axis")
+      .attr("transform", "translate(" + (7 + titleWidth) + ", 0)")
+      .call(d3.svg.axis()
+          .scale(yScale)  // スケールを適用する
+          .tickValues(function(){
+            return _.range(0, data.length * 2);
+          })
+          .tickFormat(function(d,i){
+            if (i % 2 > 0){
+              cnt += 1;
+              return data[cnt - 1][0];
+            }else{
+              return "";
+            }
+          })
+          .orient("left")
+          .tickPadding(5)
+          .innerTickSize(0)  // 目盛線の長さ（内側）
+          .outerTickSize(0) // 目盛線の長さ（外側）
+      );
+
+      svg.selectAll("rect")   // SVGでの四角形を示す要素を指定
+          .data(data) // データを設定
+          .enter()
+          .append("rect") // SVGでの四角形を示す要素を生成
+          .attr("stroke", "white") // 円グラフの区切り線を白色にする
+          .attr("x", titleWidth)   // 横棒グラフなのでX座標は0。これはSVG上での座標
+          .attr("y", function(d,i){   // Y座標を配列の順序に応じて計算
+              return top + ((data.length - i) * yBarSize) - yBarSize;
+          })
+          .attr("width", function(d){ // 横幅を配列の内容に応じて計算
+              return (d[1] * me.barScale) +"px";
+          })
+          .attr("height", yBarSize)   // 棒グラフの高さを指定
+          // .attr("style", "fill:rgb(255,0,0)") // 棒グラフの色を赤色に設定
+          .attr("transform", "translate(8, 0)")
+          .style("fill", function(d, i) {
+            return d[2];
+          })
+        me.svg = svg;
+    }
+  },
+  _redraw : {
+    value : function(data) {
+
+      var me = this;
+      var svg = me.svg;
+
+      // データを更新する処理
+      svg.selectAll("rect")   // SVGでの四角形を示す要素を指定
+        .data(data) // データを設定
+        .transition()
+        .attr("width", function(d){ // 横幅を配列の内容に応じて計算
+          return (d[1] * me.barScale) +"px";
+        })
+
+    }
+  }
+});
+
+// LineChart
+  function LineChart(option) {
+
+    Chart.call(this, option);
+
+  };
+
+  LineChart.prototype = Object.create(Chart.prototype, {
+    constructor : {
+      value : PieChart,
+      enumerable : false,
+      writable : true,
+      configurable : true
+    },
+    // グラフ作成
+    _create : {
+      value : function(data) {
+
+        var me = this;
+        var titleWidth = 50;
+        var width = me.opt.width - 20 - titleWidth; // 実際の出力サイズ(最後のメモリ分だけバッファを作成）
+
+        // 領域のリセット
+        $(me.opt.dom).empty();
+
+        // 規定オブジェクト生成
+        var svg = d3.select(me.opt.dom).append("svg").attr("width", me.opt.width).attr("height", me.opt.height);
+
+        // タイトル描画
+        var top = 0;
+        if (me.opt.title.caption) {
+          svg.append("text").attr("class", "title").attr("type", "caption").attr(
+              "x", (me.opt.width / 2)).attr("y", me.opt.title.height).attr(
+              "text-anchor", "middle").style("font-size",
+              me.opt.title.fontSize + "px").style("text-decoration", "underline")
+              .text(me.opt.title.caption);
+          top = me.opt.title.height +4;
+        }
+
+        var scaleHeight = data.length * me.opt.axisStyle.width + top;
+
+        // 目盛りを表示するためにスケールを設定
+        var xScale = d3.scale.linear()  // スケールを設定
+            .domain([0, data.length - 1])   // 元のサイズ
+            .range([0, width]);
+
+        var yScale = d3.scale.linear()  // スケールを設定
+            .domain([0, me.opt.axisStyle.maxValue])   // 元のサイズ
+            .range([scaleHeight,top]);
+
+
+        // 目盛りを設定し表示する
+        svg.append("g")
+        .attr("class", "axis")
+        .attr("transform", "translate(" + (7 + titleWidth) + ", "+scaleHeight+")")
+//        .attr("transform", "translate(" + (8 + titleWidth) + ", "+scaleHeight+")")
+        .call(d3.svg.axis()
+            .scale(xScale)  // スケールを適用する
+            .tickValues(function(){
+              return _.range(0, data.length);
+            })
+            .tickFormat(function(d,i){
+              return data[d][0];
+            })
+            .orient("bottom")   // 目盛りの表示位置を下側に指定
+            .tickPadding(5)
+            .innerTickSize(-(scaleHeight - top))  // 目盛線の長さ（内側）
+            .outerTickSize(0) // 目盛線の長さ（外側）
+        );
+
+        svg.append("g")
+            .attr("class", "axis")
+           .attr("transform", "translate(" + (7 + titleWidth) + ", 0)")
+            .call(d3.svg.axis()
+                .scale(yScale)  // スケールを適用する
+                .orient("left")
+                .tickPadding(5)
+                .innerTickSize(-width)  // 目盛線の長さ（内側）
+                .outerTickSize(0) // 目盛線の長さ（外側）
+            );
+
+        me.line = d3.svg.line()
+          .x(function(d, i){
+              return titleWidth + 7 + (i * (width/(data.length-1)));
+            })  // 横方向はSVG領域に合わせて調整。データは最低2個あるのが前提
+          .y(function(d){
+              return (scaleHeight - top)-(d[1] * ((scaleHeight - top) / me.opt.axisStyle.maxValue)) + top;
+           });  // 縦方向は数値そのままでスケール等しない
+
+          // 折れ線グラフを描画
+        me.linePath = svg.append("path")
+          .attr("d", me.line(data))  // 線を描画
+          .attr("stroke", "black")    // 線の色を指定
+          .attr("fill", "none");  // 塗り潰しなし。指定しないと黒色で塗り潰される
+
+          me.svg = svg;
+      }
+    },
+    _redraw : {
+      value : function(data) {
+
+        var me = this;
+        var svg = me.svg;
+
+        me.linePath
+        .transition()
+        .duration(800)
+        .attr('d', me.line(data));
+
+
+      }
+    }
+  });
+
+//ArrayLineChart
+  function ArrayLineChart(option) {
+
+    Chart.call(this, option);
+
+  };
+
+  ArrayLineChart.prototype = Object.create(Chart.prototype, {
+    constructor : {
+      value : PieChart,
+      enumerable : false,
+      writable : true,
+      configurable : true
+    },
+    // グラフ作成
+    _create : {
+      value : function(data) {
+
+        var me = this;
+        var titleWidth = 50;
+        var width = me.opt.width - 20 - titleWidth; // 実際の出力サイズ(最後のメモリ分だけバッファを作成）
+
+        // 領域のリセット
+        $(me.opt.dom).empty();
+
+        // 規定オブジェクト生成
+        var svg = d3.select(me.opt.dom).append("svg").attr("width", me.opt.width).attr("height", me.opt.height);
+
+        // タイトル描画
+        var top = 0;
+        if (me.opt.title.caption) {
+          svg.append("text").attr("class", "title").attr("type", "caption").attr(
+              "x", (me.opt.width / 2)).attr("y", me.opt.title.height).attr(
+              "text-anchor", "middle").style("font-size",
+              me.opt.title.fontSize + "px").style("text-decoration", "underline")
+              .text(me.opt.title.caption);
+          top = me.opt.title.height +4;
+        }
+
+        var scaleHeight = data.caption.length * me.opt.axisStyle.width + top;
+
+        // 目盛りを表示するためにスケールを設定
+        var xScale = d3.scale.linear()  // スケールを設定
+            .domain([0, data.caption.length - 1])   // 元のサイズ
+            .range([0, width]);
+
+        var yScale = d3.scale.linear()  // スケールを設定
+            .domain([0, me.opt.axisStyle.maxValue])   // 元のサイズ
+            .range([scaleHeight,top]);
+
+
+        // 目盛りを設定し表示する
+        svg.append("g")
+        .attr("class", "axis")
+        .attr("transform", "translate(" + (7 + titleWidth) + ", "+scaleHeight+")")
+//        .attr("transform", "translate(" + (8 + titleWidth) + ", "+scaleHeight+")")
+        .call(d3.svg.axis()
+            .scale(xScale)  // スケールを適用する
+            .tickValues(function(){
+              return _.range(0, data.caption.length);
+            })
+            .tickFormat(function(d,i){
+              return data.caption[d];
+            })
+            .orient("bottom")   // 目盛りの表示位置を下側に指定
+            .tickPadding(5)
+            .innerTickSize(-(scaleHeight - top))  // 目盛線の長さ（内側）
+            .outerTickSize(0) // 目盛線の長さ（外側）
+        );
+
+
+        svg.append("g")
+            .attr("class", "axis")
+           .attr("transform", "translate(" + (7 + titleWidth) + ", 0)")
+            .call(d3.svg.axis()
+                .scale(yScale)  // スケールを適用する
+                .orient("left")
+                .tickPadding(5)
+                .innerTickSize(-width)  // 目盛線の長さ（内側）
+                .outerTickSize(0) // 目盛線の長さ（外側）
+            );
+
+        me.linePath = new Array (data.data.length);
+        me.line = new Array (data.data.length);
+
+        for (var i=0;i<data.data.length;i++){
+          me.line[i] = d3.svg.line()
+            .x(function(d, k){
+              return titleWidth + 7 + (k * (width/(data.caption.length-1)));
+            })  // 横方向はSVG領域に合わせて調整。データは最低2個あるのが前提
+            .y(function(d){
+              return (scaleHeight - top)-(d * ((scaleHeight - top) / me.opt.axisStyle.maxValue)) + top;
+            });  // 縦方向は数値そのままでスケール等しない
+
+          // 折れ線グラフを描画
+        me.linePath[i] = svg.append("path")
+          .attr("d", me.line[i](data.data[i]))  // 線を描画
+          .attr("stroke", data.color[i])    // 線の色を指定
+          .attr("fill", "none");  // 塗り潰しなし。指定しないと黒色で塗り潰される
+
+          me.svg = svg;
+        }
+      }
+    },
+    _redraw : {
+      value : function(data) {
+
+        var me = this;
+        var svg = me.svg;
+
+        for (var i=0;i<data.data.length;i++){
+          me.linePath[i]
+          .transition()
+          .duration(800)
+          .attr('d', me.line[i](data.data[i]));
+        }
+
+      }
+    }
+  });
+
--- client/js/main.js
+++ client/js/main.js
@@ -8,15 +8,26 @@ define([ 'bootstrap', 'underscore', 'backbone', 'd3', 'chart' ],
           [ "項目3", "20", "#7b6888" ], [ "項目4", "60", "#6b486b" ],
           [ "項目5", "15", "#a05d56" ] ];
 
+      var list_Line = {
+          caption : ["","1/25","2/25","3/25","4/25"]
+          ,color:["#98abc5","#a05d56", "#8a89a6"]
+          ,data:[
+                 [10,30,40,20,30]
+                 ,[100,20,30,50,60]
+                 ,[100,20,30,50,60]
+                 ]
+      }
+
       var me = this;
-      me.chartArray = new Array(10);
+      me.chartArray = new Array(12);
       for (var i = 0; i < me.chartArray.length; i++) {
         index = i + 1;
         item = "chart" + i;
         $("#result").append(
             '<div id=\"' + item + '\" style=\"float:left\"></div>');
 
-        me.chartArray[i] = createChartObject("#" + item, "サンプルチャート" + index);
+        me.chartArray[i] = createChartObject("#" + item, "サンプルチャート" + index,
+            i % 4);
       }
 
       // tab-change
@@ -24,40 +35,126 @@ define([ 'bootstrap', 'underscore', 'backbone', 'd3', 'chart' ],
         var activated_tab = e.target; // activated tab
         var previous_tab = e.relatedTarget; // previous tab
         if (activated_tab.hash == "#menu2") {
-          drawChart(list);
+          drawChart(list,list_Line);
         }
       });
 
       $("#dataChange").on("click", function() {
-        drawChart(list);
+        drawChart(list,list_Line);
       });
 
-      function createChartObject(dom, caption) {
+      function createChartObject(dom, caption, chartType) {
 
-        return new PieChart({
-          dom : dom,
-          title : {
-            caption : caption
-          },
-          width : 200,
-          height : 200,
-          sorted : false
-        });
+        if (chartType == 1) {
+          return new PieChart({
+            dom : dom,
+            title : {
+              caption : caption
+            },
+            width : 200,
+            height : 200,
+            sorted : false
+          });
+        } else if (chartType == 0) {
+          return new HorizontalBarChart({
+            dom : dom,
+            title : {
+              caption : caption
+            },
+            width : 300,
+            height : 300,
+            sorted : false,
+            axisStyle : {
+              width : 32,
+              maxValue : 200
+            }
+          });
 
+        } else if (chartType == 2) {
+          return new LineChart({
+            dom : dom,
+            title : {
+              caption : caption
+            },
+            width : 300,
+            height : 300,
+            sorted : false,
+            axisStyle : {
+              width : 32,
+              maxValue : 200
+            }
+          });
+        } else {
+
+          return new ArrayLineChart({
+            dom : dom,
+            title : {
+              caption : caption
+            },
+            width : 300,
+            height : 300,
+            sorted : false,
+            axisStyle : {
+              width : 32,
+              maxValue : 200
+            }
+          });
+
+        }
       }
 
-      function drawChart(data) {
+      function drawChart(data,dataArr) {
         // 処理,,,,,
+        var list;
         for (var i = 0; i < me.chartArray.length; i++) {
-          me.chartArray[i].draw(changeData(data));
+          if (i % 4 == 0) {
+            list = changeData(data);
+            me.chartArray[i].draw(changeItemName(list,0));
+            if (i + 1 < me.chartArray.length) {
+              me.chartArray[i + 1].draw(changeItemName(list,0));
+            }
+            if (i + 2 < me.chartArray.length) {
+              me.chartArray[i + 2].draw(changeItemName(list,1));
+            }
+            if (i + 3 < me.chartArray.length) {
+              me.chartArray[i + 3].draw(changeData2(dataArr));
+            }
+          }
+        }
+      }
+
+      function changeItemName(list,tp) {
+         for (var i = 0; i < list.length; i++) {
+           list[i][0] =  getItemName(i,tp);
+        }
+        return list;
+      }
+
+      function getItemName(index,tp){
+        if (tp==0){
+          return "項目" + (index + 1);
+        }else{
+          if (i==0){
+            return "";
+          }else{
+            return index + "/25";
+          }
         }
       }
 
       function changeData(list) {
         for (var i = 0; i < list.length; i++) {
-          list[i][1] = getRandomInt(10, 1000);
+          list[i][1] = getRandomInt(0, 200);
         }
-        ;
+        return list;
+      }
+
+      function changeData2(list) {
+        for (var i = 0; i < list.data.length;i++){
+          for (var k = 0; k < list.data[i].length; k++) {
+            list.data[i][k] = getRandomInt(0, 200);
+          }
+        }
         return list;
       }
 
